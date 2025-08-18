288b75e565efb445671c6acc22a02712dd760846
SkyArrow256 <86900503+SkyArrow256@users.noreply.github.com>
2025-08-19 01:56:20 +0900

    負の単項演算子とトークンを実装

--- examples/interpreter_calc.rs
+++ examples/interpreter_calc.rs
@@ -1,3 +1,4 @@
+use infix_to_rpn::{InfixExpression, RpnExpression, Token};
 use std::io::{self, Write};
 
 fn main() {
@@ -6,44 +7,45 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut infix = String::new();
         io::stdin().read_line(&mut infix).unwrap();
-        let rpn = infix_to_rpn::to_rpn(&infix);
-        match rpn {
-            Ok(str) => println!("{}", calc(&str).unwrap()),
+        let code: Result<RpnExpression, _> = InfixExpression::from(infix.as_str()).try_into();
+        match code {
+            Ok(code) => match calc(code) {
+                Ok(num) => println!("{num}"),
+                Err(e) => println!("{e:?}"),
+            },
             Err(e) => println!("{e:?}"),
         }
     }
 }
 
-fn calc(exp: &str) -> Option<i32> {
-    let mut stack = Stack::new();
-    for elem in exp.split_whitespace() {
-        let c = elem.chars().next().unwrap();
-        if c.is_ascii_digit() {
-            stack.push(elem.parse().unwrap());
-        } else {
-            match c {
-                '+' => stack.add(),
-                '-' => stack.sub(),
-                '*' => stack.mul(),
-                '/' => stack.div(),
-                '%' => stack.modulo(),
-                '^' => stack.pow(),
-                _ => return None,
-            }
-        }
-    }
-    Some(stack.ret())
+fn calc(code: RpnExpression) -> Result<i32, CalcErr> {
+	let mut stack = Stack::new();
+	for token in code.tokens() {
+		match token {
+			Token::UnarySub => stack.unary_sub(),
+			Token::Add => stack.add(),
+			Token::Sub => stack.sub(),
+			Token::Mul => stack.mul(),
+			Token::Div => stack.div(),
+			Token::Mod => stack.modulo(),
+			Token::Pow => stack.pow(),
+			Token::Int(num) => stack.push(*num),
+			Token::Exp(_exp) => (),
+			_ => unreachable!(),
+		}
+	}
+    Ok(stack.ret())
 }
 
 struct Stack {
-    stack: [i32; 8],
+    stack: [i32; 16],
     ptr: usize,
 }
 
 impl Stack {
     fn new() -> Self {
         Self {
-            stack: [0; 8],
+            stack: [0; 16],
             ptr: 0,
         }
     }
@@ -75,7 +77,13 @@ impl Stack {
         self.stack[self.ptr - 2] = self.stack[self.ptr - 2].pow(self.stack[self.ptr - 1] as u32);
         self.ptr -= 1;
     }
+	fn unary_sub(&mut self) {
+		self.stack[self.ptr - 1] *= -1;
+	}
     fn ret(&self) -> i32 {
         self.stack[0]
     }
 }
+
+#[derive(Debug)]
+enum CalcErr {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
-//! #infix-to-rpn
+//! # infix-to-rpn
 //! 
+//! **現在多くの機能が未実装です！**
+//!
 //! infix-to-rpnは操車場アルゴリズムを使用して中置記法( ( 1 + 2 ) * 3 ^ 2 )を逆ポーランド記法( 1 2 + 3 2 ^ * )に置き換えるライブラリです。
-//! Rustではライブラリクレートとして、C言語のライブラリを扱える言語ならば共有ライブラリとして使用できます。
-//! HSPやC#でも動くはずです(未検証)。
-//! 
+//! C言語のライブラリを扱える言語、例えばHSPやC#でも動作するようにする予定です。
+//!
 //! 仕様はWikipediaを参考にしました。
-//! https://ja.wikipedia.org/wiki/操車場アルゴリズム
-//! 
-//! ##使い方
-//! to_rpn関数に中置記法の式を渡すとResult<String, ParseError>型で帰ってきます。
-//! 
-//! ##使用可能なオペレーター
-//! + - * / % ( )
-//! 
-//! ##優先度
+//! <https://ja.wikipedia.org/wiki/操車場アルゴリズム>
+//!
+//! ## 使い方
+//! &strから中置記法の式をfrom()メソッドでトークナイズしたあと、try_fromで逆ポーランド記法に変換します。
+//! 文字列はスペースが間に複数入っていても問題ありません。
+//! ```
+//! let infix = "-1 / (1+2) + -3^3"
+//! let code: Result<RpnExpression, _> = InfixExpression::from(infix).try_into();
+//! ```
+//!
+//! ## 使用できるオペレーターとその優先度
 //! 高い順に
-//! ^
-//! * / %
-//! + -
-//! です。 
+//! ``` -(単項), ^, * / %, + -(二項), :(代入) ```
+//! です。
+
+use std::mem;
 
 /// オペレーターとオペランドの間にスペースを挿入します。
 fn format(input: &str) -> String {
@@ -39,11 +42,11 @@ fn format(input: &str) -> String {
 
 /// 入力された中置記法の文字列を逆ポーランド記法に変換します。
 pub fn to_rpn(input: &str) -> Result<String, ParseError> {
-	let input = format(input);
+    let input = format(input);
     let mut que = Que::with_capacity(input.len());
     let mut stack = Vec::new();
     for str in input.split_whitespace() {
-		//空白は絶対に取り除かれているのでunwrap
+        //空白は絶対に取り除かれているのでunwrap
         match str.chars().next().unwrap() {
             //左括弧はそのままスタックに追加
             '(' => stack.push('('),
@@ -60,7 +63,9 @@ pub fn to_rpn(input: &str) -> Result<String, ParseError> {
                 //乗算、除算未満の優先度をもつ演算子は + - また、括弧でも終了する
                 while !stack.is_empty() {
                     let last_item = *stack.last().unwrap();
-                    if last_item == '(' || last_item == '+' || last_item == '-' { break; }
+                    if last_item == '(' || last_item == '+' || last_item == '-' {
+                        break;
+                    }
                     que.push_operator(stack.pop().unwrap());
                 }
                 stack.push(c);
@@ -70,7 +75,9 @@ pub fn to_rpn(input: &str) -> Result<String, ParseError> {
                 //加算、減算は優先度が最も低いので括弧だけ考える
                 while !stack.is_empty() {
                     let last_item = *stack.last().unwrap();
-                    if last_item == '(' { break; }
+                    if last_item == '(' {
+                        break;
+                    }
                     que.push_operator(stack.pop().unwrap());
                 }
                 stack.push(c);
@@ -80,7 +87,9 @@ pub fn to_rpn(input: &str) -> Result<String, ParseError> {
             _ => que.push_operand(str),
         }
     }
-    if stack.contains(&'(') { return Err(ParseError::ParenthesesMismatch) }
+    if stack.contains(&'(') {
+        return Err(ParseError::ParenthesesMismatch);
+    }
     for item in stack.iter().rev() {
         que.push_operator(*item);
     }
@@ -90,8 +99,10 @@ pub fn to_rpn(input: &str) -> Result<String, ParseError> {
 /// このライブラリで起こりうるエラーです。
 #[derive(Debug)]
 pub enum ParseError {
-	///括弧の不一致
-	ParenthesesMismatch,
+    /// 括弧の不一致
+    ParenthesesMismatch,
+    /// 数値のパースに失敗
+    InvalidNumber,
 }
 
 /// フォーマットに便利なメソッドを備えた配列です。
@@ -113,5 +124,203 @@ impl Que {
     fn get_result(mut self) -> String {
         self.0.pop();
         self.0
+    }
+}
+
+/// 式に含まれるトークンです。
+/// 式(Exp)にこれらのトークンに使われる文字を含まないでください。
+/// 例えば、次の式は正しくパースされません
+/// ```Int(3) + 1```
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    /// 左括弧 (
+    ParenLeft,
+    /// 右括弧 )
+    ParenRight,
+    /// 整数型
+    Int(i32),
+    /// 式(変数名、関数名)
+    Exp(String),
+    /// 代入 :
+    Assign,
+    /// 加算 +
+    Add,
+    /// 減算 -
+    Sub,
+    /// 乗算 *
+    Mul,
+    /// 除算 /
+    Div,
+    /// 余剰 %
+    Mod,
+    /// 単項マイナス -
+    UnarySub,
+    /// 冪乗 ^
+    Pow,
+}
+
+/// 中置記法で表現された式です。
+pub struct InfixExpression(Vec<Token>);
+
+impl InfixExpression {
+    /// Tokensへの参照を取得します。
+    fn tokens(&self) -> &[Token] {
+        &self.0
+    }
+}
+
+impl From<&str> for InfixExpression {
+    fn from(code: &str) -> Self {
+        use Token::*;
+        let mut tokens = Vec::new();
+
+        //文字を1つ受け取って式にトークンを追加 空白はスキップ
+        for c in code.chars().filter(|c| !c.is_whitespace()) {
+            if c.is_ascii_digit() {
+                //文字が数字で、最後のトークンがIntの時、最後のトークンに数字を足す
+                let num = c.to_digit(10).unwrap() as i32;
+                if let Some(last) = tokens.last_mut() {
+                    if let Int(last) = last {
+                        //10進数なので10ずらす
+                        *last *= 10;
+                        *last += num;
+                        continue;
+                    }
+                }
+                tokens.push(Int(num));
+            } else {
+                //文字が演算子のとき
+                match c {
+                    '(' => tokens.push(ParenLeft),
+                    ')' => tokens.push(ParenRight),
+                    '+' => tokens.push(Add),
+                    '-' => {
+                        /*
+                            減算として扱われるのは、直前が
+                            ・数字・式の時
+                            ・右括弧の時
+                        */
+                        if let Some(first) = tokens.last() {
+                            let last = mem::discriminant(first);
+                            if last == mem::discriminant(&ParenRight)
+                                || last == mem::discriminant(&Int(0))
+                                || last == mem::discriminant(&Exp("".to_string()))
+                            {
+                                tokens.push(Sub);
+                                continue;
+                            }
+                        }
+                        tokens.push(UnarySub);
+                    }
+                    '*' => tokens.push(Mul),
+                    '/' => tokens.push(Div),
+                    '%' => tokens.push(Mod),
+                    '^' => tokens.push(Pow),
+                    ':' => tokens.push(Assign),
+                    _ => {
+                        //文字がそれ以外、つまり式のとき、tokensの最後が式だったならそれに文字を加える
+                        if let Some(last) = tokens.last_mut() {
+                            if let Exp(str) = last {
+                                str.push(c);
+                                continue;
+                            }
+                        }
+                        tokens.push(Exp(String::from(c)));
+                    }
+                }
+            }
+        }
+        Self(tokens)
+    }
+}
+
+/// 逆ポーランド記法で表現された式です。
+#[derive(Debug)]
+pub struct RpnExpression(Vec<Token>);
+
+impl TryFrom<InfixExpression> for RpnExpression {
+    type Error = ParseError;
+
+    fn try_from(infix: InfixExpression) -> Result<Self, Self::Error> {
+        use Token::*;
+        let mut que = Vec::new();
+        let mut stack = Vec::new();
+        for token in infix.tokens() {
+            match token {
+                Assign => {
+                    //右結合なので、Assign以外の全てをpopする
+                    while let Some(last) = stack.last() {
+                        if *last == Assign {
+                            break;
+                        }
+                        que.push(stack.pop().unwrap());
+                    }
+                    stack.push(Assign);
+                }
+                //左括弧はstackに
+                ParenLeft => stack.push(ParenLeft),
+                ParenRight => {
+                    //閉じ括弧が現れるstackのtokenをqueに追加
+                    while *stack.last().ok_or(ParseError::ParenthesesMismatch)? != ParenLeft {
+                        que.push(stack.pop().unwrap());
+                    }
+                    //閉じ括弧は不要なのでstackから捨てる
+                    stack.pop();
+                }
+                c @ Add | c @ Sub => {
+                    //stackの最後が空または括弧または自分未満の優先順位を持つ演算子になるまでpopしてqueに追加
+                    while let Some(last) = stack.last() {
+                        if *last == ParenLeft || *last == Assign {
+                            break;
+                        }
+                        que.push(stack.pop().unwrap());
+                    }
+                    stack.push(c.clone());
+                }
+                c @ Mul | c @ Div | c @ Mod => {
+                    //stackの最後が空または括弧または自分未満の優先順位を持つ演算子になるまでpopしてqueに追加
+                    while let Some(last) = stack.last() {
+                        if *last == ParenLeft || *last == Assign || *last == Add || *last == Sub {
+                            break;
+                        }
+                        que.push(stack.pop().unwrap());
+                    }
+                    stack.push(c.clone());
+                }
+                Pow => {
+                    //stackの最後が空または括弧または自分**以下**の(右結合なので)優先順位を持つ演算子になるまでpopしてqueに追加
+                    //これは単項マイナス以外の全て
+                    while let Some(last) = stack.last() {
+                        if !(*last == UnarySub) {
+                            break;
+                        }
+                        que.push(stack.pop().unwrap());
+                    }
+                    stack.push(Pow);
+                }
+                //単項マイナスは最も優先順位が高いのでいつでもqueに追加
+                UnarySub => stack.push(UnarySub),
+                //その他の場合、式なのでqueにそのまま追加
+                exp @ _ => que.push(exp.clone()),
+            }
+        }
+
+        //stackに括弧が残っているなら括弧の数が間違っている
+        //数があっていたらstackの中のトークンを最後から取り出してqueに入れて終了
+        if stack.contains(&ParenLeft) {
+            Err(ParseError::ParenthesesMismatch)
+        } else {
+            for token in stack.into_iter().rev() {
+                que.push(token);
+            }
+            Ok(Self(que))
+        }
+    }
+}
+
+impl RpnExpression {
+    /// Tokensへの参照を取得します。
+    pub fn tokens(&self) -> &[Token] {
+        &self.0
     }
 }
