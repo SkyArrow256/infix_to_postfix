587de575d75e1e2e4fa97fe4bd347e0e9e6b6452
SkyArrow256 <86900503+SkyArrow256@users.noreply.github.com>
2025-08-19 09:35:42 +0900

    トークンでなく文字列で変換する関数やサンプルコードを削除

--- src/lib.rs
+++ src/lib.rs
@@ -23,79 +23,6 @@
 
 use std::mem;
 
-/// オペレーターとオペランドの間にスペースを挿入します。
-fn format(input: &str) -> String {
-    let mut result = String::new();
-    for c in input.chars() {
-        match c {
-            '+' | '-' | '*' | '/' | '^' | '(' | ')' | '%' => {
-                result.push(' ');
-                result.push(c);
-                result.push(' ');
-            }
-            _ => result.push(c),
-        }
-    }
-    //前後のスペースを削除
-    result.trim().to_string()
-}
-
-/// 入力された中置記法の文字列を逆ポーランド記法に変換します。
-pub fn to_rpn(input: &str) -> Result<String, ParseError> {
-    let input = format(input);
-    let mut que = Que::with_capacity(input.len());
-    let mut stack = Vec::new();
-    for str in input.split_whitespace() {
-        //空白は絶対に取り除かれているのでunwrap
-        match str.chars().next().unwrap() {
-            //左括弧はそのままスタックに追加
-            '(' => stack.push('('),
-            ')' => {
-                //括弧が閉じるまでスタックの中身をキューに追加
-                while *stack.iter().last().ok_or(ParseError::ParenthesesMismatch)? != '(' {
-                    que.push_operator(stack.pop().unwrap());
-                }
-                //スタックから不要な括弧は捨てる
-                stack.pop();
-            }
-            c @ '*' | c @ '/' | c @ '%' => {
-                //スタックに何かあれば、スタックの最後が自分未満の優先順位になるまでキューにスタックの中身を追加
-                //乗算、除算未満の優先度をもつ演算子は + - また、括弧でも終了する
-                while !stack.is_empty() {
-                    let last_item = *stack.last().unwrap();
-                    if last_item == '(' || last_item == '+' || last_item == '-' {
-                        break;
-                    }
-                    que.push_operator(stack.pop().unwrap());
-                }
-                stack.push(c);
-            }
-            c @ '+' | c @ '-' => {
-                //スタックに何かあれば、スタックの最後が自分未満の優先順位になるまでキューにスタックの中身を追加
-                //加算、減算は優先度が最も低いので括弧だけ考える
-                while !stack.is_empty() {
-                    let last_item = *stack.last().unwrap();
-                    if last_item == '(' {
-                        break;
-                    }
-                    que.push_operator(stack.pop().unwrap());
-                }
-                stack.push(c);
-            }
-            //累乗は右結合かつ最も優先順位が高いのでいかなる場合でもそのままキューに追加
-            '^' => stack.push('^'),
-            _ => que.push_operand(str),
-        }
-    }
-    if stack.contains(&'(') {
-        return Err(ParseError::ParenthesesMismatch);
-    }
-    for item in stack.iter().rev() {
-        que.push_operator(*item);
-    }
-    Ok(que.get_result())
-}
-
 /// このライブラリで起こりうるエラーです。
 #[derive(Debug)]
 pub enum ParseError {
@@ -103,28 +30,6 @@ pub enum ParseError {
     ParenthesesMismatch,
     /// 数値のパースに失敗
     InvalidNumber,
-}
-
-/// フォーマットに便利なメソッドを備えた配列です。
-#[derive(Debug)]
-struct Que(String);
-
-impl Que {
-    fn with_capacity(capacity: usize) -> Self {
-        Self(String::with_capacity(capacity))
-    }
-    fn push_operator(&mut self, c: char) {
-        self.0.push(c);
-        self.0.push(' ');
-    }
-    fn push_operand(&mut self, str: &str) {
-        self.0.push_str(str);
-        self.0.push(' ');
-    }
-    fn get_result(mut self) -> String {
-        self.0.pop();
-        self.0
-    }
 }
 
 /// 式に含まれるトークンです。
